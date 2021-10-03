288826981ac529b3cde9a97df38d4f14ee054e32
Gabriel Rodriguez Monteiro <gabriel.rodriguez.monteiro@ibm.com>
2021-10-03 13:35:53 -0400

    creating a shared module

--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,16 +1,13 @@
 
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { SharedModule } from "../shared/shared.module";
 
 import { CurrentTrainingComponent } from "./current-training/current-training.component";
 import { StopTrainingComponent } from "./current-training/stop-training.component";
 import { NewTrainingComponent } from "./new-training/new-training.component";
 import { PastTrainingsComponent } from "./past-trainings/past-trainings.component";
 import { TrainingComponent } from "./training.component";
-import { MaterialModule } from "../material.module";
 
 @NgModule({
   declarations: [
@@ -21,14 +18,9 @@ import { MaterialModule } from "../material.module";
     StopTrainingComponent
   ],
   imports: [
-    CommonModule,
-    FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    SharedModule
   ],
-  exports: [],
   entryComponents: [StopTrainingComponent]
 })
 export class TrainingModule {
